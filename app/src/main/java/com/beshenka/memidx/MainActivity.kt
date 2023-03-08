b87f2e414e28367ca60030ad16b5d014126bfd42
Beshenka <mambatamba6@gmail.com>
2023-03-08 11:44:54 +0300

    first commit

---
+++ app/src/main/java/com/beshenka/memidx/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beshenka.memidx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
